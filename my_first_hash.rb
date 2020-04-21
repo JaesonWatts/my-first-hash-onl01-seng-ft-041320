f45e8d203346a2d787b0f6d6ee8b83d4151aa6f7
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-21 09:06:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  new_hash = {key_key: value_value}
+  new_hash = {}
   
 end
 
